2b37c8a8d05fdf341e13ce0cbadd9139333c72b9
evando <evando.junior@live.com>
2021-07-06 00:48:22 -0300

    Feat(Products): Add the migration file

---
+++ src/migrations/1625117071227-create-products-table.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateProductsTable1625117071228 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'color',
+            type: 'varchar',
+          },
+          {
+            name: 'size',
+            type: 'varchar',
+          },
+          {
+            name: 'price',
+            type: 'double',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('products');
+  }
+}
