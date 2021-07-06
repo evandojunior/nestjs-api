72a2011bc5139feec29bd77224a7aa37e4777fb1
evando <evando.junior@live.com>
2021-07-06 00:47:02 -0300

    Feat(Products): Implementing the Products resources

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,20 @@
+import { IsNotEmpty, IsNumber, IsString, MaxLength } from 'class-validator';
+
+export class CreateProductDto {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
+
+  @IsString()
+  @IsNotEmpty()
+  size: string;
+
+  @IsString()
+  @IsNotEmpty()
+  color: string;
+}
